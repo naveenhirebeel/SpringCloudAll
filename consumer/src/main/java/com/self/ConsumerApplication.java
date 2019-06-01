b43d71cdfd6165d4c7924a569449eccf09944534
Naveen Hirebeel <naveen.s.hirebeel@boeing.com>
2019-06-01 19:04:27 +0530

    Working copy of producer / consumer / feign Client / Zuul Gateway

--- consumer/src/main/java/com/self/ConsumerApplication.java
+++ consumer/src/main/java/com/self/ConsumerApplication.java
@@ -1,13 +1,9 @@
 package com.self;
 
-import com.self.client.EmployeeClient;
-import com.self.model.Employee;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
